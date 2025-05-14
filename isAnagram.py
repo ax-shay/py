355546adc6069470fe547a1e07d9a822e408ffc5
Shembekar, Akshay P <akshay.p.shembekar@jpmorgan.com>
2025-05-14 13:44:59 -0400

    Adding code to check if the two strings are anagrams

---
+++ isAnagram.py
@@ -0,0 +1,17 @@
+class Solution:
+   def isAnagram(self, s: str, t: str) -> bool:
+      s_dict ={}
+      t_dict ={}
+      if len(s) != len(t):
+         return False
+      else:
+         for i in range(len(s)):
+            s_dict[s[i]] = s_dict.get(s[i],0) + 1
+            t_dict[t[i]] = t_dict.get(t[i],0) + 1
+         if s_dict == t_dict:
+            return True
+      return False
+
+if __name__ == '__main__':
+   s = Solution()
+   print(s.isAnagram("acr","arc"))
